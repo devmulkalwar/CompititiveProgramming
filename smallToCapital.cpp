91ee6454c2f9d2b739e187b4e6fa2fd951a181c3
Dev Mulkalwar <devmulkalwar95@gmail.com>
2024-12-21 23:48:29 +0530

    Create smallToCapital.cpp

---
+++ smallToCapital.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string : ";
+   cin>>str;
+   
+   for(int i=0; i<str.length();i++){
+       if(str[i]>=65 && str[i]<=90){
+           continue;
+       }
+       else{
+          int temp = int(str[i]) - 32;
+          str[i] = char(temp);
+       }
+   }
+   cout<< str;
+    return 0;
+}
