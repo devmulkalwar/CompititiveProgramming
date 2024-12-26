edff6cb9c53761ce8a9875a5863f1cb95279b395
Dev Mulkalwar <devmulkalwar95@gmail.com>
2024-12-26 22:20:12 +0530

    Updated factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -2,27 +2,26 @@
 using namespace std;
 
 int factorial(int n) {
-    int fact = 1;
     if (n == 0 || n == 1) {
-        return fact;
+        return 1;
     } else {
-        fact = n * factorial(n - 1);
+        return n * factorial(n - 1);
     }
-    return fact;
 }
 
 int main() {
     int n = 0;
 
-    cout << "Enter a number :" << endl;
+    cout << "Enter a number: ";
     cin >> n;
 
     if (n < 0) {
         cout << "Factorial is not defined for negative numbers." << endl;
-    } else {
-        int fact = factorial(n);
-        cout << "The factorial of " << n << " is " << fact << endl;
+        return 0; // Exit the program gracefully
     }
+
+    int fact = factorial(n);
+    cout << "The factorial of " << n << " is " << fact << endl;
 
     return 0;
 }
